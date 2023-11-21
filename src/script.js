26e8e96cd9eb9bc6d5d810be66e03fc97e0705a2
Juliana Nunes <jununes95@gmail.com>
2023-11-20 22:22:52 -0300

    Acrescentado um comentário no script

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 var nome = prompt("Como você se chama?");
 var dolar = prompt("Qual valor em dolar você quer converter em real? ");
 
+//cotacao do dolar em 13/11/23
 var cotacaoDoDolar = 5.32;
 
 var valorEmReal = dolar * cotacaoDoDolar;
